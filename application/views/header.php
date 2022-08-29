ec38836807582a321336a3dfc054763b3094d048
jose <josecastanedauni@gmail.com>
2022-08-29 16:54:39 -0600

    Se agrega el proyecto

---
+++ application/views/header.php
@@ -0,0 +1,85 @@
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href=<?= base_url("template/images/favicon.png")?> type="">
+
+  <title> Mont </title>
+  
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href=<?= base_url("template/css/bootstrap.css")?> />
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href=<?= base_url("template/css/owl.carousel.min.css")?> />
+
+  <!-- font awesome style -->
+  <link href=<?= base_url("template/css/font-awesome.min.css")?> rel="stylesheet" />
+  <!-- lightbox -->
+  <link href=<?= base_url("template/css/ekko-lightbox.css")?> rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href=<?= base_url("template/css/style.css")?> rel="stylesheet" />
+  <!-- responsive style -->
+  <link href=<?= base_url("template/css/responsive.css")?> rel="stylesheet" />
+
+</head>
+<div class="hero_area">
+ <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Mont
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  ">
+              <li class="nav-item active">
+                <a class="nav-link" href="#l">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"> About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"> Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Gallery</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  
+    <!-- end header section -->
+
+     <!-- jQery -->
+  <script src=<?= base_url("template/js/jquery-3.4.1.min.js")?>></script>
+  <!-- popper js -->
+  <script src=<?= base_url("template/js/popper.min.js")?> integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script src=<?= base_url("template/js/bootstrap.js")?>></script>
+  <!-- owl slider -->
+  <script src=<?= base_url("template/js/owl.carousel.min.js")?>>
+  </script>
+  <!-- lightbox -->
+  <script src=<?= base_url("template/js/ekko-lightbox.min.js")?>></script>
+  <!-- custom js -->
+  <script src=<?= base_url("template/js/custom.js")?>></script>
+  <!-- Google Map -->
+  <script src=<?= base_url("template/js/js.js")?>>
+  </script>
---
+++ application/views/inicio.php
@@ -0,0 +1,190 @@
+ <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href=<?= base_url("template/css/bootstrap.css")?> />
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href=<?= base_url("template/css/owl.carousel.min.css")?> />
+
+  <!-- font awesome style -->
+  <link href=<?= base_url("template/css/font-awesome.min.css")?> rel="stylesheet" />
+  <!-- lightbox -->
+  <link href=<?= base_url("template/css/ekko-lightbox.css")?> rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href=<?= base_url("template/css/style.css")?> rel="stylesheet" />
+  <!-- responsive style -->
+  <link href=<?= base_url("template/css/responsive.css")?> rel="stylesheet" />
+
+    <!-- slider section -->
+    <section class="slider_section ">
+      <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7 col-lg-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Best Tattoos
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-5 col-lg-6">
+                  <div class="img-box col-lg-10 mx-auto px-0">
+                    <img src=<?= base_url("template/images/mont.png")?> alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item ">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7 col-lg-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Best Tattoos
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-5 col-lg-6">
+                  <div class="img-box col-lg-10 mx-auto px-0">
+                    <img src=<?= base_url("template/images/mont.png")?> alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7 col-lg-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Best Tattoos
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-5 col-lg-6">
+                  <div class="img-box col-lg-10 mx-auto px-0">
+                    <img src=<?= base_url("template/images/mont.png")?> alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <ol class="carousel-indicators">
+            <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+            <li data-target="#customCarousel1" data-slide-to="1"></li>
+            <li data-target="#customCarousel1" data-slide-to="2"></li>
+          </ol>
+        </div>
+      </div>
+
+    </section>
+    <!-- end slider section -->
+  </div>
+
+  <!-- service section -->
+
+  <section class="service_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          What We Do
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="box">
+            <div class="img-box">
+              <img src=<?= base_url("template/images/s1.png")?> alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Tattooing
+              </h5>
+              <p>
+                Odio vero voluptatibus excepturi in, dolor neque nesciunt reiciendis saepe veniam, pariatur fuga, nam voluptatum minima id? Quod omnis nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box">
+            <div class="img-box">
+              <img src=<?= base_url("template/images/s2.png")?> alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Piercing
+              </h5>
+              <p>
+                Odio vero voluptatibus excepturi in, dolor neque nesciunt reiciendis saepe veniam, pariatur fuga, nam voluptatum minima id? Quod omnis nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box">
+            <div class="img-box">
+              <img src=<?= base_url("template/images/s3.png")?> alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Tattoo Design
+              </h5>
+              <p>
+                Odio vero voluptatibus excepturi in, dolor neque nesciunt reiciendis saepe veniam, pariatur fuga, nam voluptatum minima id? Quod omnis nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end service section -->
+
+    <!-- jQery -->
+  <script src=<?= base_url("template/js/jquery-3.4.1.min.js")?>></script>
+  <!-- popper js -->
+  <script src=<?= base_url("template/js/popper.min.js")?> integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script src=<?= base_url("template/js/bootstrap.js")?>></script>
+  <!-- owl slider -->
+  <script src=<?= base_url("template/js/owl.carousel.min.js")?>>
+  </script>
+  <!-- lightbox -->
+  <script src=<?= base_url("template/js/ekko-lightbox.min.js")?>></script>
+  <!-- custom js -->
+  <script src=<?= base_url("template/js/custom.js")?>></script>
+  <!-- Google Map -->
+  <script src=<?= base_url("template/js/js.js")?>>
+  </script>
+
